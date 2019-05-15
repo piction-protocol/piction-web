396423fd0664ee1cb53ffbf19fa01a605693117d
안도현 <dh.ahn@battleent.com>
2019-05-15 19:03:24 +0900

    문서 주소 수정

--- src/components/organisms/Editor/index.jsx
+++ src/components/organisms/Editor/index.jsx
@@ -3,18 +3,26 @@ import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
 function Editor({ ...props }) {
-  function handleChange(c, d) {
-    console.log(c);
-    console.log(JSON.stringify(d, null, 2));
+  function handleChange(content) {
+    console.log(content);
   }
 
   return (
     <ReactQuill
       theme="snow"
-      onChange={(content, delta, source, editor) => handleChange(content, editor.getContents())}
+      onChange={content => handleChange(content)}
       {...props}
     />
   );
 }
+
+Editor.defaultProps = {
+  modules: {
+    toolbar: [
+      ['bold', 'italic', 'underline', { align: [] }],
+      ['image', 'link'],
+    ],
+  },
+};
 
 export default Editor;
