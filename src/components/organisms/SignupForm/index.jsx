1527cf1a678878fca5e94a52d021d69a6defad98
안도현 <dh.ahn@battleent.com>
2019-06-03 12:20:26 +0900

    회원가입 POST 구현

--- src/components/organisms/SignupForm/index.jsx
+++ src/components/organisms/SignupForm/index.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
 import Heading from 'components/atoms/Heading';
 import { PrimaryButton } from 'components/atoms/Button';
@@ -38,38 +39,84 @@ const Styled = {
   `,
 };
 
-const SignupForm = () => (
-  <Styled.Form>
-    <Styled.Heading>
-      회원가입
-    </Styled.Heading>
-    <Styled.InputGroup
-      id="email"
-      label="이메일"
-      placeholder="이메일을 입력해주세요."
-    />
-    <Styled.InputGroup
-      id="password"
-      label="비밀번호"
-      placeholder="6자 이상의 비밀번호"
-      type="password"
-    />
-    <Styled.InputGroup
-      id="username"
-      label="닉네임"
-      placeholder="2자 이상의 닉네임"
-    />
-    <Styled.Notice>
-      Piction 베타 서비스는 테스트 용도로 운영되며,
-      <Styled.Em>
-        실제 메인넷 환경의 토큰을 사용하는 것이 아닙니다.
-      </Styled.Em>
-      테스트 목적에 따라 데이터가 초기화되거나 롤백될 수 있으며, 테스터가 등록한 데이터 손실에 대한 책임은 Piction에서 지지 않습니다.
-    </Styled.Notice>
-    <Styled.Submit
-      value="동의 및 회원가입"
-    />
-  </Styled.Form>
-);
+function SignupForm() {
+  const [newUser, setNewUser] = useState({
+    email: '',
+    password: '',
+    username: '',
+  });
+
+  const handleChange = (value, name) => {
+    setNewUser({ ...newUser, [name]: value });
+  };
+
+  const storeAccessToken = (token) => {
+    sessionStorage.setItem('access-token', token);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios({
+      method: 'post',
+      url: 'http://api-iro.piction.network/users',
+      data: newUser,
+      headers: {
+        accept: '*/*',
+        'Content-Type': 'application/json',
+      },
+    }).then((response) => {
+      storeAccessToken(response.data.accessToken);
+    }).catch((error) => {
+      // 에러 메시지 처리
+      console.log(error);
+    });
+  };
+
+  return (
+    <Styled.Form onSubmit={handleSubmit}>
+      <Styled.Heading>
+        회원가입
+      </Styled.Heading>
+      <Styled.InputGroup
+        id="email"
+        label="이메일"
+        placeholder="이메일을 입력해주세요."
+        autoComplete="email"
+        value={newUser.email}
+        onChange={event => handleChange(event.target.value, 'email')}
+        required
+      />
+      <Styled.InputGroup
+        id="password"
+        label="비밀번호"
+        placeholder="6자 이상의 비밀번호"
+        type="password"
+        autoComplete="new-password"
+        value={newUser.password}
+        onChange={event => handleChange(event.target.value, 'password')}
+        required
+      />
+      <Styled.InputGroup
+        id="username"
+        label="닉네임"
+        placeholder="2자 이상의 닉네임"
+        autoComplete="nickname"
+        value={newUser.username}
+        onChange={event => handleChange(event.target.value, 'username')}
+        required
+      />
+      <Styled.Notice>
+        Piction 베타 서비스는 테스트 용도로 운영되며,
+        <Styled.Em>
+          실제 메인넷 환경의 토큰을 사용하는 것이 아닙니다.
+        </Styled.Em>
+        테스트 목적에 따라 데이터가 초기화되거나 롤백될 수 있으며, 테스터가 등록한 데이터 손실에 대한 책임은 Piction에서 지지 않습니다.
+      </Styled.Notice>
+      <Styled.Submit
+        value="동의 및 회원가입"
+      />
+    </Styled.Form>
+  );
+}
 
 export default SignupForm;
