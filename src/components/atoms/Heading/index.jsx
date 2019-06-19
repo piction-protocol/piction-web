afee65a500642ee27a5f706b5b54001c7d36befa
안도현 <dh.ahn@battleent.com>
2019-06-19 11:52:18 +0900

    Heading에 페이지 Title 변경 기능 추가

--- src/components/atoms/Heading/index.jsx
+++ src/components/atoms/Heading/index.jsx
@@ -1,8 +1,33 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const Heading = styled.h1`
-  font-size: var(--font-size--large);
-  font-weight: bold;
-`;
+const Styled = {
+  Heading: styled.h1`
+    font-size: var(--font-size--large);
+    font-weight: bold;
+  `,
+};
+
+function Heading({ children, className }) {
+  return (
+    <Styled.Heading className={className}>
+      {children}
+      <Helmet>
+        <title>{`${children} - Piction Beta`}</title>
+      </Helmet>
+    </Styled.Heading>
+  );
+}
+
+Heading.propTypes = {
+  children: PropTypes.string.isRequired,
+  className: PropTypes.string,
+};
+
+Heading.defaultProps = {
+  className: '',
+};
 
 export default Heading;
