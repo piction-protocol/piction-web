ac004533f5a54d10b7a91f35ee13cd63a473189f
안도현 <dh.ahn@battleent.com>
2019-07-03 18:54:51 +0900

    대시보드 Redirect 처리 개선

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -15,9 +15,7 @@ const NotFound = () => (
   <div>개발중</div>
 );
 
-const Project = ({ children }) => (
-  <div>{children}</div>
-);
+const Project = ({ children }) => children;
 
 Project.propTypes = {
   children: PropTypes.node.isRequired,
@@ -25,6 +23,7 @@ Project.propTypes = {
 
 function Dashboard() {
   const [projects, setProjects] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
   const [API] = useAPI();
 
   useEffect(() => {
@@ -34,6 +33,8 @@ function Dashboard() {
         await setProjects(data);
       } catch (error) {
         console.log(error);
+      } finally {
+        setIsLoaded(true);
       }
     };
 
@@ -43,16 +44,18 @@ function Dashboard() {
 
   return (
     <DashboardTemplate projects={projects}>
-      <Router primary={false}>
-        <Redirect from="/" to={[].length ? 'dashboard/1' : 'dashboard/new-project'} noThrow />
-        <ProjectForm title="새 프로젝트" path="new-project" setProjects={setProjects} />
-        <Project path=":projectId">
-          <ProjectForm title="프로젝트 정보 수정" path="info" setProjects={setProjects} />
-          <PostForm title="새 포스트" path="posts/new" />
-          <PostForm title="포스트 수정" path="posts/:postId/edit" />
-          <NotFound default />
-        </Project>
-      </Router>
+      {isLoaded && (
+        <Router primary={false}>
+          <Redirect from="/" to={projects.length ? `dashboard/${projects[0].uri}` : 'dashboard/new-project'} noThrow />
+          <ProjectForm title="새 프로젝트" path="new-project" setProjects={setProjects} />
+          <Project path=":projectId">
+            <ProjectForm title="프로젝트 정보 수정" path="info" setProjects={setProjects} />
+            <PostForm title="새 포스트" path="posts/new" />
+            <PostForm title="포스트 수정" path="posts/:postId/edit" />
+            <NotFound default />
+          </Project>
+        </Router>
+      )}
     </DashboardTemplate>
   );
 }
