a1dd2967edc3973d94214dd2cc107a271647b050
안도현 <dh.ahn@battleent.com>
2019-07-05 17:15:07 +0900

    Add PostList

---
+++ src/components/molecules/PostItem/index.jsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import media from 'styles/media';
+
+import { ReactComponent as ThumbupIcon } from 'images/ic-thumbup.svg';
+
+import ContentImage from 'components/atoms/ContentImage';
+
+const Styled = {
+  Item: styled.article`
+    display: flex;
+    flex-flow: column;
+    padding-bottom: 24px;
+    border-bottom: 1px solid var(--gray--light);
+    background-color: var(--white);
+  `,
+  Cover: styled(ContentImage)`
+    margin-bottom: 16px;
+  `,
+  Title: styled.h2`
+    margin-bottom: 4px;
+    font-size: var(--font-size--small);
+    ${media.desktop`
+      font-size: var(--font-size--base);
+    `}
+  `,
+  Text: styled.div`
+    display: flex;
+    align-items: flex-end;
+  `,
+  CreatedAt: styled.p`
+    color: var(--gray--dark);
+    font-size: var(--font-size--small);
+  `,
+  LikeCount: styled.span`
+    display: flex;
+    align-items: center;
+    margin-left: auto;
+    color: var(--gray--dark);
+    font-size: var(--font-size--small);
+    ${media.desktop`
+      margin-right: 24px;
+    `}
+  `,
+  ThumbupIcon: styled(ThumbupIcon)`
+    margin-right: 4px;
+  `,
+};
+
+function dateConverter(time) {
+  const parsedTimeString = time.replace(
+    /([+-])([0-9]{2})([0-9]{2})\b/,
+    (params, p1, p2, p3) => `${p1}${p2}:${p3}`,
+  );
+  const date = new Date(parsedTimeString);
+
+  return `${(date.getMonth() + 1)}월 ${date.getDate()}일`;
+}
+
+function PostItem({
+  title, cover, createdAt, likeCount, ...props
+}) {
+  const createdDate = dateConverter(createdAt);
+  return (
+    <Styled.Item
+      {...props}
+    >
+      {cover && (
+        <Styled.Cover
+          ratio={960 / 360}
+          image={cover}
+        />
+      )}
+      <Styled.Title>{title}</Styled.Title>
+      <Styled.Text>
+        <Styled.CreatedAt>
+          {createdDate}
+        </Styled.CreatedAt>
+        <Styled.LikeCount>
+          <Styled.ThumbupIcon />
+          {likeCount}
+        </Styled.LikeCount>
+      </Styled.Text>
+    </Styled.Item>
+  );
+}
+
+PostItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  cover: PropTypes.string,
+  createdAt: PropTypes.string.isRequired,
+  likeCount: PropTypes.number,
+};
+
+PostItem.defaultProps = {
+  cover: null,
+  likeCount: 0,
+};
+
+export default PostItem;
---
+++ src/components/organisms/PostList/index.jsx
@@ -0,0 +1,100 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Link } from '@reach/router';
+
+import useAPI from 'hooks/useAPI';
+
+import media from 'styles/media';
+
+import { ReactComponent as BadMoodIcon } from 'images/ic-mood-bad.svg';
+
+import PostItem from 'components/molecules/PostItem';
+import { PrimaryButton } from 'components/atoms/Button';
+
+const Styled = {
+  Container: styled.div`
+    display: flex;
+    flex-flow: column;
+    flex: 1;
+  `,
+  List: styled.div`
+    display: flex;
+    flex-flow: column;
+    > * {
+      margin-bottom: 24px;
+      ${media.desktop`
+        margin-bottom: 48px;
+      `}
+    }
+  `,
+  Empty: styled.div`
+    margin: 80px auto;
+    color: var(--gray--dark);
+    font-size: var(--font-size--base);
+    text-align: center;
+  `,
+  BadMoodIcon: styled(BadMoodIcon)`
+    width: 160px;
+    height: 160px;
+  `,
+  More: styled(PrimaryButton)`
+    margin-right: auto;
+    margin-left: auto;
+  `,
+};
+
+function PostList({ projectId, ...props }) {
+  const [page, setPage] = useState(1);
+  const [isLast, setIsLast] = useState(true);
+  const [contentList, setContentList] = useState([]);
+  const [API] = useCallback(useAPI(), []);
+
+  useEffect(() => {
+    const getFormData = async () => {
+      try {
+        const { data } = await API.post(projectId).getAll({ params: { size: 10, page } });
+        setContentList(prevContentList => [
+          ...prevContentList,
+          ...data.content,
+        ]);
+        setIsLast(data.last);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getFormData();
+  }, [API, projectId, page]);
+
+  return (
+    <Styled.Container {...props}>
+      {contentList.length === 0 && (
+        <Styled.Empty>
+          <Styled.BadMoodIcon />
+          <p>
+            등록된 포스트가 없습니다.
+          </p>
+        </Styled.Empty>
+      )}
+      <Styled.List>
+        {contentList.map(content => (
+          <Link to={`posts/${content.id}`} key={content.id}>
+            <PostItem {...content} />
+          </Link>
+        ))}
+        {!isLast && (
+          <Styled.More onClick={() => setPage(prevPage => (prevPage + 1))}>
+            포스트 더 보기
+          </Styled.More>
+        )}
+      </Styled.List>
+    </Styled.Container>
+  );
+}
+
+PostList.propTypes = {
+  projectId: PropTypes.string.isRequired,
+};
+
+export default PostList;
