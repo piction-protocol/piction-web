49363b6776fbe7b7a223accf58795e841b1e8817
안도현 <dh.ahn@battleent.com>
2019-07-10 17:15:40 +0900

    대시보드 스타일 수정

--- src/components/templates/DashboardTemplate/index.jsx
+++ src/components/templates/DashboardTemplate/index.jsx
@@ -2,18 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+import media from 'styles/media';
 import { MainGrid } from 'styles/Grid';
 
 import DashboardSidebar from 'components/organisms/DashboardSidebar';
 
 const Styled = {
   Sidebar: styled(DashboardSidebar)`
-    grid-column: 1 / span 3;
+    grid-column: 1 / -1;
+    ${media.desktop`
+      grid-column: 1 / span 3;
+    `}
   `,
   Content: styled.section`
     display: flex;
     flex-flow: column;
-    grid-column: span 9;
+    grid-column: 1 / -1;
+    ${media.desktop`
+      grid-column: span 9;
+    `}
   `,
 };
 
