8923c697ef49e49a779dff2a0e6b5849100618d8
안도현 <dh.ahn@battleent.com>
2019-07-11 13:56:00 +0900

    푸터 스타일, 링크 변경

--- src/components/organisms/GlobalFooter/index.jsx
+++ src/components/organisms/GlobalFooter/index.jsx
@@ -3,13 +3,13 @@ import styled from 'styled-components';
 
 import media from 'styles/media';
 
-import { ReactComponent as Logo } from 'images/piction-logo--mini.svg';
+import { ReactComponent as Logo } from 'images/img-piction-logo--gray.svg';
 
 const Styled = {
   Footer: styled.footer`
     display: flex;
     margin-top: auto;
-    padding: var(--outer-gap) 0;
+    padding: var(--row-gap) 0;
     border-top: 1px solid var(--gray--light);
     background-color: var(--white);
     ${media.desktop`
@@ -24,33 +24,52 @@ const Styled = {
     max-width: var(--max-width);
     height: 100%;
     margin: auto;
+    padding: 0 var(--outer-gap);
     ${media.desktop`
       flex-flow: row wrap;
-      padding: 0 var(--column-gap);
     `}
   `,
   Logo: styled(Logo)`
-    width: 36px;
-    ${media.mobile`
-      display: none;
+    width: 104px;
+    height: auto;
+    margin-top: 16px;
+    ${media.desktop`
+      order: -1;
+      width: 152px;
+      margin-top: 0;
     `}
   `,
-  List: styled.ul`
+  Links: styled.ul`
     display: flex;
     flex-flow: row wrap;
     justify-content: center;
-    padding: 0 16px;
     ${media.desktop`
       justify-content: flex-start;
+      margin-left: 32px;
+    `}
+    > *:not(:last-child) {
+      margin-right: 16px;
+      ${media.desktop`
+        margin-right: 32px;
+      `}
+    }
+  `,
+  Texts: styled.ul`
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    ${media.desktop`
+      flex-flow: column;
+      margin-left: auto;
+      text-align: right;
     `}
   `,
   Item: styled.li`
-    margin: 4px 8px;
+    margin-bottom: 10px;
     color: var(--gray--dark);
     font-size: var(--font-size--small);
-    text-align: center;
     ${media.desktop`
-      margin: 4px 16px;
+      margin: 4px 0;
     `}
   `,
   Link: styled.a.attrs({
@@ -58,46 +77,40 @@ const Styled = {
   })`
     color: inherit;
   `,
+  Em: styled.em`
+    color: var(--black);
+    font-style: normal;
+  `,
 };
 
 function GlobalFooter() {
   return (
     <Styled.Footer>
       <Styled.Wrapper>
+        <Styled.Links>
+          <Styled.Item>
+            <Styled.Link href="https://about.piction.network">
+              About
+            </Styled.Link>
+          </Styled.Item>
+          <Styled.Item>
+            <Styled.Link href="/terms">
+              서비스 이용약관
+            </Styled.Link>
+          </Styled.Item>
+          <Styled.Item>
+            <Styled.Link href="/privacy">
+              <Styled.Em>
+                개인정보 처리방침
+              </Styled.Em>
+            </Styled.Link>
+          </Styled.Item>
+        </Styled.Links>
+        <Styled.Texts>
+          <Styled.Item>Contact | contact@piction.network</Styled.Item>
+          <Styled.Item>© 2018-2019 Piction Network. All rights reserved</Styled.Item>
+        </Styled.Texts>
         <Styled.Logo />
-        <div>
-          <Styled.List>
-            <Styled.Item>
-              <Styled.Link href="https://t.me/pictionnetwork">
-                Telegram Channel
-              </Styled.Link>
-            </Styled.Item>
-            <Styled.Item>
-              <Styled.Link href="https://open.kakao.com/o/gyVQbDM">
-                KaKaotalk
-              </Styled.Link>
-            </Styled.Item>
-            <Styled.Item>
-              <Styled.Link href="https://medium.com/piction">
-                Medium
-              </Styled.Link>
-            </Styled.Item>
-            <Styled.Item>
-              <Styled.Link href="https://github.com/piction-protocol">
-                Github
-              </Styled.Link>
-            </Styled.Item>
-            <Styled.Item>
-              <Styled.Link href="https://www.youtube.com/channel/UCzT4fqmHbnrgJDB1eMjQUbA">
-                Youtube
-              </Styled.Link>
-            </Styled.Item>
-          </Styled.List>
-          <Styled.List>
-            <Styled.Item>Contact | contact@piction.network</Styled.Item>
-            <Styled.Item>© 2018-2019 Piction Network. All rights reserved</Styled.Item>
-          </Styled.List>
-        </div>
       </Styled.Wrapper>
     </Styled.Footer>
   );
