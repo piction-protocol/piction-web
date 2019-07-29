0504c9f152122d39421692239332541a304b826b
안도현 <dh.ahn@battleent.com>
2019-07-29 19:07:31 +0900

    UserMenu 링크 아이콘 추가

--- src/components/molecules/UserMenu/index.jsx
+++ src/components/molecules/UserMenu/index.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Link } from '@reach/router';
+
+import useAPI from 'hooks/useAPI';
 
 const Styled = {
   Wallet: styled.div`
@@ -12,22 +14,34 @@ const Styled = {
     border-bottom: 1px solid var(--gray--light);
     color: var(--black);
     font-size: var(--font-size--small);
+    font-weight: bold;
   `,
   PXL: styled.span`
+    margin-top: 4px;
     color: var(--blue);
+    font-size: var(--font-size--base);
   `,
   Links: styled.div`
     display: flex;
     flex-flow: column;
-    align-items: flex-start;
-    padding: 16px;
   `,
   Link: styled(Link)`
+    display: flex;
+    align-items: center;
+    padding: 12px 16px;
     color: var(--black);
     font-size: var(--font-size--small);
     cursor: pointer;
-    &:not(:last-child) {
-      margin-bottom: 16px;
+    transition: background-color var(--transition--form);
+
+    svg {
+      width: 20px;
+      height: 20px;
+      margin-right: 8px;
+    }
+
+    &:hover {
+      background-color: var(--gray--light);
     }
   `,
 };
@@ -35,6 +49,16 @@ const Styled = {
 function UserMenu({
   links, PXL,
 }) {
+  const [API] = useCallback(useAPI(), []);
+  const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    const getProjects = async () => {
+      const { data } = await API.my.projects();
+      setProjects(data);
+    };
+
+    getProjects();
+  }, [API]);
   return (
     <>
       <Styled.Wallet>
@@ -43,11 +67,20 @@ function UserMenu({
           {`${PXL.toLocaleString()} PXL`}
         </Styled.PXL>
       </Styled.Wallet>
+      {projects.length > 0 && (
+        <Styled.Wallet>
+          내 프로젝트
+          {projects.map(project => (
+            project.title
+          ))}
+        </Styled.Wallet>
+      )}
       <Styled.Links>
         {links.map(({
-          text, ...link
+          icon, text, ...link
         }) => (
           <Styled.Link key={text} {...link}>
+            {icon}
             {text}
           </Styled.Link>
         ))}
