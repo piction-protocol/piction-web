71b78fcbf73c6b83a285e3850619280c3b914722
안도현 <dh.ahn@battleent.com>
2019-08-12 18:20:35 +0900

    새 프로젝트 생성 불가 페이지 추가

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Router, Redirect } from '@reach/router';
+import styled from 'styled-components';
 
 import useAPI from 'hooks/useAPI';
 
@@ -7,12 +8,47 @@ import withLoginChecker from 'components/LoginChecker';
 
 import DashboardTemplate from 'components/templates/DashboardTemplate';
 
+import { ReactComponent as BadMoodIcon } from 'images/ic-mood-bad.svg';
+
 const ProjectForm = React.lazy(() => import('components/organisms/ProjectForm'));
 const PostForm = React.lazy(() => import('components/organisms/PostForm'));
 const DashboardPostList = React.lazy(() => import('components/organisms/DashboardPostList'));
 
+const Styled = {
+  Router: styled(Router)`
+    display: flex;
+    height: 100%;
+  `,
+  Container: styled.section`
+    margin: auto;
+    color: var(--gray--dark);
+    font-size: var(--font-size--base);
+    text-align: center;
+  `,
+  BadMoodIcon: styled(BadMoodIcon)`
+    width: 160px;
+    height: 160px;
+  `,
+};
+
+const NoMoreProject = () => (
+  <Styled.Container>
+    <Styled.BadMoodIcon />
+    <p>
+      최대 프로젝트 보유 가능 수를 초과하여
+      <br />
+      새 프로젝트를 만들 수 없습니다.
+    </p>
+  </Styled.Container>
+);
+
 const NotFound = () => (
-  <div>개발중</div>
+  <Styled.Container>
+    <Styled.BadMoodIcon />
+    <p>
+      페이지를 찾을 수 없습니다.
+    </p>
+  </Styled.Container>
 );
 
 function Dashboard() {
@@ -25,8 +61,6 @@ function Dashboard() {
       try {
         const { data } = await API.my.projects();
         await setProjects(data);
-      } catch (error) {
-        console.log(error);
       } finally {
         setIsLoaded(true);
       }
@@ -39,9 +73,12 @@ function Dashboard() {
   return (
     <DashboardTemplate projects={projects}>
       {isLoaded && (
-        <Router primary={false}>
+        <Styled.Router primary={false}>
           <Redirect from="/" to={projects.length ? `dashboard/${projects[0].uri}/posts` : 'dashboard/new-project'} noThrow />
-          <ProjectForm title="새 프로젝트" path="new-project" setProjects={setProjects} />
+          {projects.length >= 3
+            ? <NoMoreProject path="new-project" />
+            : <ProjectForm title="새 프로젝트" path="new-project" setProjects={setProjects} />
+          }
           <ProjectForm title="프로젝트 정보 수정" path=":projectId/info" setProjects={setProjects} />
 
           <Redirect from="/:projectId" to="dashboard/:projectId/posts" noThrow />
@@ -49,7 +86,7 @@ function Dashboard() {
           <PostForm title="새 포스트" path=":projectId/posts/new" />
           <PostForm title="포스트 수정" path=":projectId/posts/:postId/edit" />
           <NotFound default />
-        </Router>
+        </Styled.Router>
       )}
     </DashboardTemplate>
   );
