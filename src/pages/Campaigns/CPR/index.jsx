a6749cbdd88574281c15a711bb8394f7f978bc40
안도현 <dh.ahn@battleent.com>
2019-10-23 18:09:16 +0900

    Add participate button

--- src/pages/Campaigns/CPR/index.jsx
+++ src/pages/Campaigns/CPR/index.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import useMedia from 'hooks/useMedia';
-
 import media, { mediaQuery } from 'styles/media';
 
 import title from './title-mobile.png';
@@ -20,6 +18,8 @@ import steps2x from './steps-mobile@2x.png';
 import steps3x from './steps-mobile@3x.png';
 import stepsDesktop from './steps-desktop.png';
 
+import { ReactComponent as ParticipateButton } from './btn-participate.svg';
+
 const Styled = {
   Article: styled.article`
     display: flex;
@@ -27,85 +27,94 @@ const Styled = {
     flex-flow: column;
     width: 100%;
   `,
+  Picture: styled.picture`
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    overflow: hidden;
+  `,
   Image: styled.img`
     width: 100%;
     height: auto;
     ${media.desktop`
+      flex-shrink: 0;
       width: auto;
     `}
   `,
-  Section: styled.section`
+  Link: styled.a.attrs({
+    target: '_blank',
+  })`
+    position: absolute;
     display: flex;
-    justify-content: center;
-    overflow: hidden;
+    ${media.mobile`
+      bottom: 0;
+      width: 80.833%;
+      margin-bottom: 83%;
+      svg {
+        width: 100%;
+        height: auto;
+      }
+    `}
+    ${media.desktop`
+      bottom: 365px;
+    `}
   `,
 };
 
 function CPR() {
-  const isMobile = useMedia(mediaQuery.mobile);
-
-  return isMobile ? (
+  return (
     <Styled.Article>
-      <Styled.Image
-        src={title}
-        srcSet={`
-          ${title} 360w,
-          ${title2x} 720w,
-          ${title3x} 1080w
-        `}
-        alt="심폐소생전 2019 픽션 창작지원 프로그램"
-      />
-      <Styled.Image
-        src={intro}
-        srcSet={`
-          ${intro} 360w,
-          ${intro2x} 720w,
-          ${intro3x} 1080w
-        `}
-        alt="당신의 서랍 속 작품. 다시 심장 뛰게 하세요."
-      />
-      <Styled.Image
-        src={steps}
-        srcSet={`
-          ${steps} 360w,
-          ${steps2x} 720w,
-          ${steps3x} 1080w
-        `}
-        alt="작품 올리고, 용돈도 꿀-꺽! 참 쉬운 참여 방법."
-      />
-    </Styled.Article>
-  ) : (
-    <Styled.Article>
-      <Styled.Section
+      <Styled.Picture
         style={{
           backgroundImage: 'linear-gradient(to bottom, #cc162f, #ff6076)',
         }}
       >
+        <source media={mediaQuery.desktop} srcSet={titleDesktop} />
         <Styled.Image
-          src={titleDesktop}
+          src={title}
+          srcSet={`
+            ${title} 360w,
+            ${title2x} 720w,
+            ${title3x} 1080w
+          `}
           alt="심폐소생전 2019 픽션 창작지원 프로그램"
         />
-      </Styled.Section>
-      <Styled.Section
+      </Styled.Picture>
+      <Styled.Picture
         style={{
           backgroundColor: '#c5273f',
         }}
       >
+        <source media={mediaQuery.desktop} srcSet={introDesktop} />
         <Styled.Image
-          src={introDesktop}
+          src={intro}
+          srcSet={`
+            ${intro} 360w,
+            ${intro2x} 720w,
+            ${intro3x} 1080w
+          `}
           alt="당신의 서랍 속 작품. 다시 심장 뛰게 하세요."
         />
-      </Styled.Section>
-      <Styled.Section
+      </Styled.Picture>
+      <Styled.Picture
         style={{
           backgroundColor: '#ffe8eb',
         }}
       >
+        <source media={mediaQuery.desktop} srcSet={stepsDesktop} />
         <Styled.Image
-          src={stepsDesktop}
+          src={steps}
+          srcSet={`
+            ${steps} 360w,
+            ${steps2x} 720w,
+            ${steps3x} 1080w
+          `}
           alt="작품 올리고, 용돈도 꿀-꺽! 참 쉬운 참여 방법."
         />
-      </Styled.Section>
+        <Styled.Link href="/dashboard/new-project">
+          <ParticipateButton />
+        </Styled.Link>
+      </Styled.Picture>
     </Styled.Article>
   );
 }
