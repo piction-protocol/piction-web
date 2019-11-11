0e624bd94cfe9508643f7677e6af0f7887ed2e80
안도현 <dh.ahn@battleent.com>
2019-11-11 16:40:59 +0900

    Add collection API to Choice section

--- src/pages/HomePage/Choice.jsx
+++ src/pages/HomePage/Choice.jsx
@@ -16,11 +16,28 @@ const Styled = {
     width: 100%;
     padding-top: 16px;
     ${media.desktop`
-      padding-top: 40px;
+      position: relative;
+      margin-bottom: 120px;
+      padding-top: 80px;
+      &::before {
+        content: '';
+        position: absolute;
+        top: 0;
+        right: 0;
+        left: 0;
+        z-index: -1;
+        height: 235px;
+        background-color: #f1f9ff;
+      }
     `}
   `,
   MainImage: styled(Thumbnail)`
     grid-column: 1 / -1;
+    ${media.desktop`
+      grid-column: 1 / 5;
+      grid-row: span 2;
+      box-shadow: 0 9px 12px 0 rgba(0, 0, 0, .15);
+    `}
   `,
   Texts: styled.div`
     ${media.mobile`
@@ -31,21 +48,36 @@ const Styled = {
       background-color: var(--white);
       text-align: center;
     `}
+    ${media.desktop`
+      grid-column: 5 / -1;
+      margin: 18px 0px 40px 20px;
+    `}
   `,
   Name: styled.p`
     margin-bottom: 8px;
     font-size: 14px;
     font-family: 'Poppins', sans-serif;
+    ${media.desktop`
+      font-size: 16px;
+    `}
   `,
   Title: styled.h2`
     margin-bottom: 8px;
-    font-size: 18px;
+    font-size: var(--font-size--base);
+    ${media.desktop`
+      margin-bottom: 12px;
+      font-size: var(--font-size--large);
+    `}
   `,
   SubTitle: styled.p`
     color: #999999;
+    ${media.desktop`
+      font-size: var(--font-size--small);
+    `}
   `,
   ProjectList: styled(Grid).attrs(() => ({
     columns: 8,
+    as: 'ul',
   }))`
     ${media.mobile`
       --column-gap: 12px;
@@ -57,6 +89,7 @@ const Styled = {
       padding-bottom: 24px;
       overflow-x: scroll;
       scroll-snap-type: x proximity;
+      scroll-padding: var(--column-gap);
       -webkit-overflow-scrolling: touch;
       &::before, &::after {
         content: '';
@@ -68,7 +101,7 @@ const Styled = {
     `}
   `,
   ProjectListItem: styled.li`
-    scroll-snap-align: center;
+    scroll-snap-align: start;
     ${media.desktop`
       grid-column: span 2;
     `}
@@ -76,6 +109,9 @@ const Styled = {
   Project: styled(Link)`
     display: flex;
     flex-flow: column;
+    ${media.desktop`
+      margin-left: 20px;
+    `}
   `,
   ProjectTitle: styled.span`
     margin-top: 12px;
@@ -84,43 +120,44 @@ const Styled = {
     font-weight: bold;
     text-overflow: ellipsis;
     white-space: nowrap;
+    ${media.desktop`
+      margin-top: 16px;
+    `}
   `,
 };
 
 const Choice = (props) => {
-  const [projects, setProjects] = useState([]);
+  const [collection, setCollection] = useState(null);
   const [API] = useCallback(useAPI(), []);
 
   useEffect(() => {
     async function fetchProject() {
-      const response = await API.recommended.getProjects({ params: { size: 4 } });
-      const fetchedProjects = response.data.map(p => ({
-        ...p,
-      }));
-      setProjects(fetchedProjects);
+      const { data } = await API.collection.getActive({ params: { size: 4 } });
+      setCollection(data);
     }
+
     fetchProject();
   }, [API]);
 
-  return (
+  return collection ? (
     <Styled.Container {...props}>
       <MainGrid>
         <Styled.MainImage
-          image="https://placekitten.com/800/800"
+          image={collection.thumbnail}
         />
         <Styled.Texts>
           <Styled.Name>
             Piction’s Choice
           </Styled.Name>
           <Styled.Title>
-            회전목마
+            {collection.title}
           </Styled.Title>
           <Styled.SubTitle>
-            말 돌리기 바빠~
+            {collection.subTitle}
           </Styled.SubTitle>
         </Styled.Texts>
         <Styled.ProjectList>
-          {projects.map(project => (
+          {collection.projects.map(project => (
             <Styled.ProjectListItem key={project.id}>
               <Styled.Project to={`/project/${project.uri}`}>
                 <Thumbnail image={project.thumbnail} />
@@ -131,7 +168,7 @@ const Choice = (props) => {
         </Styled.ProjectList>
       </MainGrid>
     </Styled.Container>
-  );
+  ) : null;
 };
 
 export default Choice;
