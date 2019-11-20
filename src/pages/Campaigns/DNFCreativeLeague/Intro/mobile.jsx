7f6ede1af842c7496193f4e2fdffd5de297e3bdf
안도현 <dh.ahn@battleent.com>
2019-11-20 16:15:14 +0900

    Update DNFCreativeLeague page

--- src/pages/Campaigns/DNFCreativeLeague/Intro/mobile.jsx
+++ src/pages/Campaigns/DNFCreativeLeague/Intro/mobile.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from '@reach/router';
 
 import titleImage from './title-mobile.png';
 import titleImage2 from './title-mobile@2x.png';
 import titleImage3 from './title-mobile@3x.png';
 
-import introImage from './intro-mobile.png';
-import introImage2 from './intro-mobile@2x.png';
-import introImage3 from './intro-mobile@3x.png';
+import Prize1stBackground from './bg-prize-1-st.jpg';
+import Prize1stImage from './img-prize-1-st.png';
+import Prize2ndImage from './img-prize-2-nd.png';
+import Prize3rdImageTitle from './img-prize-3-rd-title.png';
+import Prize3rdImage1 from './img-prize-3-rd-01.png';
+import Prize3rdImage2 from './img-prize-3-rd-02.png';
 
 const Styled = {
   TitleImage: styled.img`
@@ -15,8 +19,33 @@ const Styled = {
     top: 0;
     width: 100%;
   `,
-  IntroImage: styled.img`
-    padding-top: 64%;
+  Image: styled.img`
+    width: 100%;
+  `,
+  Prize1st: styled.div`
+    margin-top: 64%;
+    padding: 100% 5.5555% 22.222%;
+    background-color: #00213d;
+    background-image: url(${Prize1stBackground});
+    background-size: contain;
+    background-repeat: no-repeat;
+  `,
+  Prize2nd: styled.div`
+    padding: 22.222% 5.5555%;
+    background-color: #001629;
+  `,
+  Prize3rd: styled.div`
+    display: flex;
+    flex-flow: column;
+    padding: 22.222% 0 0;
+    background-color: #00213d;
+    a {
+      margin: 0 16.667% 22.222%;
+    }
+  `,
+  Prize3rdTitleImage: styled.img`
+    width: 32.5%;
+    margin: 0 auto 22.222%;
   `,
 };
 
@@ -31,15 +60,25 @@ const Intro = () => (
       `}
       alt="픽션X던파 크리에이티브 리그 2019"
     />
-    <Styled.IntroImage
-      src={introImage}
-      srcSet={`
-        ${introImage} 360w,
-        ${introImage2} 720w,
-        ${introImage3} 1080w
-      `}
-      alt="총 상금 4,000만원 규모 #던파크리, 발동!"
-    />
+    <Styled.Prize1st>
+      <Link to="/project/dolechan/posts">
+        <Styled.Image src={Prize1stImage} alt="대상 ♡도레챤 러브하우스 ♡ by 도레챤" />
+      </Link>
+    </Styled.Prize1st>
+    <Styled.Prize2nd>
+      <Link to="/project/5star/posts">
+        <Styled.Image src={Prize2ndImage} alt="최우수상 던파는 끊어도 팬아트는 못끊겠어 by 색별" />
+      </Link>
+    </Styled.Prize2nd>
+    <Styled.Prize3rd>
+      <Styled.Prize3rdTitleImage src={Prize3rdImageTitle} alt="우수상" />
+      <Link to="/project/dnf-sisters/posts">
+        <Styled.Image src={Prize3rdImage1} alt="던파의 누님들 by 남실" />
+      </Link>
+      <Link to="/project/piter100/posts">
+        <Styled.Image src={Prize3rdImage2} alt="마법사의방 by 피터지게냥" />
+      </Link>
+    </Styled.Prize3rd>
   </>
 );
 
