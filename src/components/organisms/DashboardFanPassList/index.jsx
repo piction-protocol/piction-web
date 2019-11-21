b70489161d151d2a4d5b169d0453903fc4f94881
안도현 <dh.ahn@battleent.com>
2019-11-21 17:45:11 +0900

    Add request section to DashboardFanPassList

--- src/components/organisms/DashboardFanPassList/index.jsx
+++ src/components/organisms/DashboardFanPassList/index.jsx
@@ -7,6 +7,9 @@ import Grid from 'styles/Grid';
 
 import FanPass from 'components/molecules/FanPass';
 import Heading from 'components/atoms/Heading';
+import { PrimaryButton } from 'components/atoms/Button';
+
+import { ReactComponent as FanPassImage } from './img-fanpass-null.svg';
 
 const Styled = {
   Container: styled.div`
@@ -22,24 +25,58 @@ const Styled = {
   FanPass: styled(FanPass)`
     grid-column: 1 / -1;
   `,
+  Center: styled.div`
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    margin: auto;
+    color: var(--gray--dark);
+    font-size: var(--font-size--small);
+    text-align: center;
+  `,
+  Strong: styled.strong`
+    margin: 24px auto;
+    color: var(--gray--dark);
+    font-size: var(--font-size--base);
+    font-weight: bold;
+  `,
+  Link: styled(PrimaryButton).attrs(() => ({
+    as: 'a',
+  }))`
+    margin-top: 24px;
+  `,
 };
 
 function DashboardFanPassList({ title, projectId }) {
+  const { data: project } = useSWR(`/projects/${projectId}`, { suspense: true });
   const { data: fanPassList } = useSWR(`/projects/${projectId}/fan-pass`, { suspense: true });
 
   return (
     <Styled.Container>
       <Heading>{title}</Heading>
-      {true ? (
+      {project.activeFanPass ? (
         <Grid columns={9}>
           {fanPassList.map(fanPass => (
-            <Styled.FanPass {...fanPass} />
+            <Styled.FanPass {...fanPass} key={fanPass.id} />
           ))}
         </Grid>
       ) : (
-        <div>
-          아직 유료 FAN PASS 관리 권한이 없습니다.
-        </div>
+        <Styled.Center>
+          <FanPassImage />
+          <Styled.Strong>
+            아직 유료 FAN PASS 관리 권한이 없습니다.
+          </Styled.Strong>
+          <p>
+            내 창작물로 수익을 얻는 가장 쉬운 방법, FAN PASS.
+            <br />
+            창작자라면 누구나 FAN PASS 생성이 가능합니다.
+            <br />
+            지금 바로, 부담없이 신청해보세요!
+          </p>
+          <Styled.Link target="_blank" href="#">
+            FAN PASS 권한 신청하기
+          </Styled.Link>
+        </Styled.Center>
       )}
     </Styled.Container>
   );
