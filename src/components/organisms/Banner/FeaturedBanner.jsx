b2f6910c97098137c343ffd3f0ae7442a192f90f
Jiwoong Heo <jiwoong.heo@gmail.com>
2019-12-13 11:23:12 +0900

    Remove `CPR` banner

--- src/components/organisms/Banner/FeaturedBanner.jsx
+++ src/components/organisms/Banner/FeaturedBanner.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
 
 import Banner from '.';
-import cprMobileImage from './cpr-banner-mobile.jpg';
-import cprDesktopImage from './cpr-banner-desktop.jpg';
 import hongikMobileImage from './hongik-banner-mobile.jpg';
 import hongikDesktopImage from './hongik-banner-desktop.jpg';
 
 const FeaturedBanner = () => (
   <Banner
     banners={[{
-      mobileImage: cprMobileImage,
-      desktopImage: cprDesktopImage,
-      title: '전 국민 창작지원 프로그램, 심폐소생전',
-      color: '#f45067',
-      link: '/campaigns/cpr_2019',
-    }, {
       mobileImage: hongikMobileImage,
       desktopImage: hongikDesktopImage,
       title: '픽션x홍익대 산학협력 프로젝트',
