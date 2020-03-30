bd95c89c9b483a4cbbf65eeda9e325ce71975ea8
안도현 <doh9898@gmail.com>
2020-03-30 16:03:09 +0900

    Install storybook and addons

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  stories: ['../src/**/*.stories.jsx'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-viewport/register'
+  ],
+};
