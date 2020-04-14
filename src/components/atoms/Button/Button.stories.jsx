669521122520d26f1f4e15c412597627f67aba08
안도현 <doh9898@gmail.com>
2020-04-14 18:30:44 +0900

    Revise Button story

--- src/components/atoms/Button/Button.stories.jsx
+++ src/components/atoms/Button/Button.stories.jsx
@@ -2,9 +2,23 @@ import React from 'react';
 import { PrimaryButton, SecondaryButton, TertiaryButton } from '.';
 
 export default {
-  title: 'Atoms/Button',
+  title: 'Components|Atoms/Button',
 };
 
+export const all = () => (
+  <>
+    <PrimaryButton>Primary</PrimaryButton>
+    <PrimaryButton size="mini">Primary Mini</PrimaryButton>
+    <SecondaryButton>Secondary</SecondaryButton>
+    <SecondaryButton size="mini">Secondary Mini</SecondaryButton>
+    <TertiaryButton>Tertiary</TertiaryButton>
+    <TertiaryButton size="mini">Tertiary Mini</TertiaryButton>
+  </>
+);
+
 export const primary = () => <PrimaryButton>Primary</PrimaryButton>;
+export const primaryMini = () => <PrimaryButton size="mini">Primary Mini</PrimaryButton>;
 export const secondary = () => <SecondaryButton>Secondary</SecondaryButton>;
+export const secondaryMini = () => <SecondaryButton size="mini">Secondary Mini</SecondaryButton>;
 export const tertiary = () => <TertiaryButton>Tertiary</TertiaryButton>;
+export const tertiaryMini = () => <TertiaryButton size="mini">Tertiary Mini</TertiaryButton>;
