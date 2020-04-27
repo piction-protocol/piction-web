09f95cf018d2c7d549fbd754e464ab26f817677c
vewevteen <vewevteen0414@gmail.com>
2020-04-27 18:29:35 +0900

    fix code

--- src/pages/ErrorPage/index.jsx
+++ src/pages/ErrorPage/index.jsx
@@ -1,88 +1,34 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 import { Link } from '@reach/router';
-import media from 'styles/media';
 
 import { PrimaryButton } from 'components/atoms/Button';
-import { ReactComponent as errorpage } from './img-404-error.svg';
+import { ReactComponent as oopsImg } from './img-404-error.svg';
 
 const Styled = {
   Main: styled.section`
     display: flex;
     flex-flow: column;
     align-items: center;
+    margin: auto;
+    color: var(--gray--dark);
   `,
-  Img: styled(errorpage)`
-  width: 328px;
-  height: 140px;
-  object-fit: contain;
-  ${media.desktop`
-  width: 611px;
-  height: 261px;
-    `}
+  Img: styled(oopsImg)`
+    margin-bottom: 24px;
+    object-fit: contain;
   `,
-  BottomLetter: styled.section`
-  width: 94px;
-  height: 20px;
-  font-family: NotoSansCJKkr;
-  font-size: 14px;
-  font-weight: bold;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  text-align: center;
-  color: white;
-  `,
-  Button: styled(PrimaryButton).attrs({ as: Link })`
-  margin-bottom: 8px;
-  width: 134px;
-  height: 52px;
-  `,
-  TopLetter: styled.h4`
-  width: 195px;
-  height: 31px;
-  font-family: NotoSansCJKkr;
-  font-size: 18px;
-  font-weight: normal;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: 1.72;
-  letter-spacing: normal;
-  text-align: center;
-  color: #BFBFBF;
-  `,
-  Box: styled.section`
-  padding-bottom: 50px;
-  ${media.desktop`
-  padding-bottom: 60px;
-    `}
-  `,
-  TopBox: styled.section`
-  padding-bottom: 50px;
-  padding-top: 90px;
-  ${media.desktop`
-  padding-bottom: 70px;
-  padding-top: 90px;
-    `}
+  Button: styled(PrimaryButton)`
+    margin-top: 24px;
   `,
 };
 
 function ErrorPage() {
   return (
     <Styled.Main>
-      <Styled.TopBox>
-        <Styled.Img />
-      </Styled.TopBox>
-      <Styled.Box>
-        <Styled.TopLetter>
-          페이지를 찾을 수 없습니다.
-        </Styled.TopLetter>
-      </Styled.Box>
+      <Styled.Img />
+        페이지를 찾을 수 없습니다.
       <Styled.Button as={Link} to="/">
-        <Styled.BottomLetter>
-          홈으로 돌아가기
-        </Styled.BottomLetter>
+        홈으로 돌아가기
       </Styled.Button>
     </Styled.Main>
   );
