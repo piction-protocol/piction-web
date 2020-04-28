e1871d6b9aa7ca79b67e3214debbcd41c94376f4
vewevteen <vewevteen0414@gmail.com>
2020-04-28 11:41:15 +0900

    modify to show the current number of characters

--- src/components/organisms/MembershipForm/index.jsx
+++ src/components/organisms/MembershipForm/index.jsx
@@ -46,6 +46,14 @@ const Styled = {
     margin-bottom: var(--row-gap);
     white-space: nowrap;
   `,
+  InputGroupInside: styled.div`
+    display: flex;
+    grid-row: 2;
+    grid-column: 9;
+    align-items: center;
+    color: var(--gray);
+    font-size: var(--font-size--base);
+  `,
   Unit: styled.span`
     display: flex;
     grid-row: 2;
@@ -120,6 +128,8 @@ function MembershipForm({
   const [isUnlimited, setIsUnlimited] = useState(!defaultValues.sponsorLimit);
   const [isDeletingMembership, setIsDeletingMembership] = useState(false);
   const [canDeleteMembership, setCanDeleteMembership] = useState(true);
+  const [supportTitle, setTitle] = useState('');
+  const [supportExplain, setExplain] = useState('');
 
   // Compute settlement amount when watching price or fees changed
   useEffect(() => {
@@ -176,7 +186,20 @@ function MembershipForm({
           </Styled.Level>
         </div>
       )}
-      <Styled.InputGroup inputRef={register} name="name" label="상품명" required>
+      <Styled.InputGroup
+        inputRef={register}
+        name="name"
+        label="상품명"
+        value={supportTitle}
+        onChange={({ target: { value } }) => setTitle(value)}
+        style={{
+          gridColumn: '1 / 9',
+        }}
+        required
+      >
+        <Styled.InputGroupInside>
+          {`${supportTitle.length} / 30`}
+        </Styled.InputGroupInside>
         {errors.name && (
           <ErrorMessage>
             {errors.name.message}
@@ -231,8 +254,15 @@ function MembershipForm({
         })}
         name="description"
         label="설명"
+        onChange={({ target: { value } }) => setExplain(value)}
         placeholder="최대 100자"
+        style={{
+          gridColumn: '1 / 9',
+        }}
       >
+        <Styled.InputGroupInside>
+          {`${supportExplain.length} / 100`}
+        </Styled.InputGroupInside>
         {errors.description && (
           <ErrorMessage>
             {errors.description.message}
