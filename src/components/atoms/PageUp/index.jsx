7e3de373e3e8df15ff58348b177bd092b04cb940
vewevteen <vewevteen0414@gmail.com>
2020-05-13 12:36:28 +0900

    add upButton image

--- src/components/atoms/PageUp/index.jsx
+++ src/components/atoms/PageUp/index.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
+import { ReactComponent as upButton } from './ic-arrow-upward.svg';
+
 const Styled = {
   GotoUp: styled.div`
   cursor: pointer;
+  display: flex;
+  position: fixed;
+  bottom: 40px;
+  right: 40px;
+  width: 48px;
+  height: 48px;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0 2px 20px 0 rgba(0, 0, 0, 0.15);
+  border: solid 1px #e8e8e8;
+  background-color: var(--white);
+  `,
+  upButton: styled(upButton)`
+  width: 28px;
+  height: 28px;
+  object-fit: contain;
   `,
 };
 
-const scroll = () => {
-  window.scroll({ top: 0, left: 0, behavior: 'smooth' });
-};
-
 function PageUp() {
+  const upscroll = () => {
+    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+  };
+
   return (
     <div>
-      <Styled.GotoUp className="up" onClick={scroll} role="button">위로가기</Styled.GotoUp>
+      <Styled.GotoUp className="up" onClick={upscroll} role="button" title="위로 올라가기"><Styled.upButton /></Styled.GotoUp>
     </div>
   );
 }
