fe0755e333e7b05a200afe2e63cf8a45b56b94cf
vewevteen <vewevteen0414@gmail.com>
2020-06-26 13:07:16 +0900

    fix other country default language

--- src/hooks/useLanguage.js
+++ src/hooks/useLanguage.js
@@ -2,13 +2,16 @@ function useLanguage() {
   let willRenderLanguage = navigator.language.search(/ko/);
   const koLanguage = navigator.language.search(/ko/);
   const enLanguage = navigator.language.search(/en/);
+  const cnLanguage = navigator.language.search(/cn/);
 
   if (koLanguage === 0) {
     willRenderLanguage = 'ko';
   } else if (enLanguage === 0) {
     willRenderLanguage = 'en';
-  } else {
+  } else if (cnLanguage === 0) {
     willRenderLanguage = 'cn';
+  } else {
+    willRenderLanguage = 'en';
   }
   return willRenderLanguage;
 }
