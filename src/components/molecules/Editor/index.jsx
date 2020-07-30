22852cd82ea1f2df0bc366c135d369ae96f3ddb9
안도현 <doh9898@gmail.com>
2020-07-30 18:45:07 +0900

    Ensure quill.getSelection always return range

--- src/components/molecules/Editor/index.jsx
+++ src/components/molecules/Editor/index.jsx
@@ -107,9 +107,9 @@ function Editor({
   useEffect(() => {
     if (imagesFromModal.length >= 1) {
       const quill = quillRef.current.getEditor();
-      const range = quill.getSelection();
+      const range = quill.getSelection(true);
       const urls = imagesFromModal;
-      urls.forEach(url => quill.insertEmbed(range.index, 'image', url.result));
+      urls.forEach((url, index) => quill.insertEmbed(range.index + index, 'image', url.result));
     }
   }, [imagesFromModal]);
 
